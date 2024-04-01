1bbae27a5228a57083f0d42570fa3cd0fb817597
zhaxun <zhaxun@qq.com>
2024-04-01 11:43:05 +0800

    Update abs.cpp

--- abs/abs.cpp
+++ abs/abs.cpp
@@ -6,12 +6,21 @@ private:
 public:
     abs(/* args */);
     ~abs();
+
+
+    template<T> T value(T t);
 };
 
 abs::abs(/* args */)
 {
+    
 }
 
 abs::~abs()
 {
+}
+
+T abs::value(T t)
+{
+    return  < 0 ? -t : t;
 }
